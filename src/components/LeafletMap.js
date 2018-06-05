32b7774db65f95f986cdcf6b8ea4e37e4ec6188c
Henna Kalliokoski <henna.ruohonen@helsinki.fi>
2018-06-05 10:59:05 +0300

    config tilelayer

--- src/components/LeafletMap.js
+++ src/components/LeafletMap.js
@@ -1,5 +1,5 @@
-import {Map, TileLayer} from 'react-leaflet';
-import React, { Component } from 'react';
+import {Map, TileLayer} from 'react-leaflet'
+import React, {Component} from 'react'
 
 export class LeafletMap extends Component {
   constructor() {
@@ -11,32 +11,17 @@ export class LeafletMap extends Component {
     }
   }
 
-  // initializeMap() {
-  //   this.map = L.map("map-leaflet");
-  //   L.tileLayer("https://digitransit-prod-cdn-origin.azureedge.net/map/v1/hsl-map/{z}/{x}/{y}{retina}.png", {
-  //     maxZoom: 18,
-  //     tileSize: 512,
-  //     zoomOffset: -1,
-  //     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-  //     '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-  //     'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-  //     retina: L.retina ? "" : "@2x",
-  //     baseLayer: true,
-  //   }).addTo(this.map);
-  //   addControlButton(this.map, this.props.toggleFullscreen);
-  //   addRouteFilterLayer(this.map);
-  // }
-
   render() {
     const position = [this.state.lat, this.state.lng]
-    return(
-      <Map center={position} zoom={this.state.zoom}>
+    return <Map center={position} zoom={this.state.zoom} maxZoom={18}>
         <TileLayer
-          attribution="foobar"
+          attribution={'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+          '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'}
           url="https://digitransit-prod-cdn-origin.azureedge.net/map/v1/hsl-map/{z}/{x}/{y}@2x.png"
+          tileSize={512}
+          zoomOffset={-1}
         />
-      </Map>
-    )
+    </Map>
   }
 
 
