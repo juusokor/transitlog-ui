7743510c7f827a0ab50a016bc7d7573f2986a53f
danieldunderfelt <daniel@developsuperpowers.com>
2018-07-17 14:52:40 +0300

    Undo App.js to pre-formatted state (will help avoid merge conflicts later)

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,20 @@
-import React, {Component} from "react";
-import "./App.css";
-import {LeafletMap} from "./LeafletMap";
-import {FilterPanel} from "./FilterPanel";
-import {ApolloProvider} from "react-apollo";
-import {joreClient, digiTClient, hfpClient} from "../api";
+import React, {Component} from 'react';
+import './App.css';
+import {LeafletMap} from './LeafletMap';
+import {FilterPanel} from './FilterPanel';
+import {ApolloProvider} from 'react-apollo';
+import {joreClient, digiTClient, hfpClient } from '../api';
+
 
 class App extends Component {
+  
   constructor() {
     super();
     this.state = {
-      date: "2018-05-07",
-      startTime: "11:55",
-      selectedRoute: {
-        lineId: "1006",
-        direction: "2",
-        dateBegin: "2017-08-14",
-        dateEnd: "2018-12-31",
-      },
-    };
+      date: '2018-05-07',
+      startTime: '11:55',
+      selectedRoute: {lineId: '1006', direction: '2', dateBegin: '2017-08-14', dateEnd: '2018-12-31'}
+    }
   }
 
   onRouteSelected = ({lineId, dateBegin, dateEnd}) => {
@@ -26,17 +23,14 @@ class App extends Component {
 
   render() {
     return (
-      <ApolloProvider client={joreClient}>
-        <div className="transitlog">
-          <FilterPanel
-            selectedRoute={this.state.selectedRoute}
-            onRouteSelected={this.onRouteSelected}
-          />
-          <LeafletMap selectedRoute={this.state.selectedRoute} />
-        </div>
-      </ApolloProvider>
-    );
+    <ApolloProvider client={joreClient}>
+      <div className="transitlog">
+        <FilterPanel selectedRoute={this.state.selectedRoute} onRouteSelected={this.onRouteSelected}/>
+        <LeafletMap selectedRoute={this.state.selectedRoute}/>
+      </div>
+    </ApolloProvider>
+    )
   }
 }
 
-export default App;
+export default App
