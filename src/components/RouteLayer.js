4e7b942acb29f1af8d3304435ec6441d8fd8c640
Henna Kalliokoski <henna.ruohonen@helsinki.fi>
2018-07-18 12:19:11 +0000

    stop circle

--- src/components/RouteLayer.js
+++ src/components/RouteLayer.js
@@ -4,11 +4,14 @@ import {Polyline, Circle} from "react-leaflet";
 class RouteLayer extends Component {
   coords = this.props.positions.map(([lon, lat]) => [lat, lon]);
   teststop = this.props.stops[5].stop
+  stops = this.props.stops
   render() {
     console.log('STOPP', this.teststop)
     return (
-      <Polyline weight={5} positions={this.coords} />,
-      <Circle center={[this.teststop.lat, this.teststop.lon]} color="red" radius={20} />
+      [<Polyline weight={5} positions={this.coords} />,
+      [this.stops.map(({stop}) => (
+         <Circle center={[stop.lat, stop.lon]} color="red" radius={20} />))],
+      <Circle center={[this.teststop.lat, this.teststop.lon]} color="red" radius={20} />]
     );
   }
 }
