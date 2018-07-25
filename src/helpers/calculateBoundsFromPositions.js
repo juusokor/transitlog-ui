70dab164b653003dcbaea2e7a53facd21086bf3b
Daniel Dunderfelt <daniel@developsuperpowers.com>
2018-07-25 14:45:25 +0300

    Bounds calculation WIP.

---
+++ src/helpers/calculateBoundsFromPositions.js
@@ -0,0 +1,25 @@
+import {latLngBounds} from "leaflet";
+
+function calculateBoundsFromPositions(
+  positions,
+  defaultPosition = {lat: 0, lng: 0}
+) {
+  let latMin = defaultPosition.lat;
+  let lngMin = defaultPosition.lng;
+  let latMax = latMin;
+  let lngMax = lngMin;
+
+  let posIndex = 0;
+  const totalPositions = positions.length;
+
+  for (; posIndex < totalPositions; posIndex++) {
+    const pos = positions[posIndex];
+
+    latMin = Math.min(latMin, pos.lat);
+    lngMin = Math.min(lngMin, pos.lng);
+    latMax = Math.max(latMax, pos.lat);
+    lngMax = Math.max(lngMax, pos.lng);
+  }
+
+  return latLngBounds([[latMin, lngMin], [latMax, lngMax]]);
+}
