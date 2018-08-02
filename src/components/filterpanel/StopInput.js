6ed1a130096bd3b4a387d12519701894eff2fa39
Daniel Dunderfelt <daniel@developsuperpowers.com>
2018-08-02 14:28:00 +0300

    SHow stop name and enable searching by the name.

--- src/components/filterpanel/StopInput.js
+++ src/components/filterpanel/StopInput.js
@@ -6,7 +6,9 @@ import intersection from "lodash/intersection";
 import uniq from "lodash/uniq";
 
 const getSuggestionValue = (suggestion) =>
-  suggestion.stopId ? `${suggestion.shortId} (${suggestion.stopId})` : "";
+  suggestion.stopId
+    ? `${suggestion.shortId} - ${suggestion.nameFi} (${suggestion.stopId})`
+    : "";
 
 const renderSuggestion = (suggestion) => (
   <span className="suggestion-content">
@@ -15,8 +17,13 @@ const renderSuggestion = (suggestion) => (
 );
 
 const suggestionFitness = (inputValue) => (stop) => {
-  const inputArr = inputValue.match(/[\d]/g).filter((char) => char !== " ");
-  const stopIdArr = (stop.shortId + stop.stopId).match(/[\d]/g);
+  const inputArr = !isNaN(inputValue)
+    ? inputValue.match(/[\d]/g).filter((char) => char !== " ")
+    : inputValue.toLowerCase().split();
+
+  const stopIdArr = !isNaN(inputValue)
+    ? (stop.shortId + stop.stopId).match(/[\d]/g)
+    : stop.nameFi.toLowerCase().split();
 
   const fitnessScore = inputArr.reduce((score, val, index) => {
     const foundIndex = stopIdArr.indexOf(val, index);
@@ -34,12 +41,18 @@ const getSuggestions = (stops = []) => (value = "") => {
     inputLength === 0 || stops.length === 0
       ? stops
       : stops.filter((item) =>
-          fuzzySearch(inputValue, (item.shortId + item.stopId).match(/\d/g).join(""))
+          fuzzySearch(
+            inputValue,
+            !isNaN(inputValue)
+              ? (item.shortId + item.stopId).match(/\d/g).join("")
+              : item.nameFi
+          )
         );
 
   return orderBy(
     suggestionStops.slice(0, 100),
-    inputValue ? suggestionFitness(inputValue) : "stopId"
+    inputValue ? suggestionFitness(inputValue) : "stopId",
+    "desc"
   );
 };
 
