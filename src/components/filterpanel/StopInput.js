4365334f38659fc17a07eb24f427ae09d1ea8bb0
Henna Kalliokoski <henna.ruohonen@helsinki.fi>
2018-08-30 10:48:43 +0000

    fix stop shortid filter and clean up

--- src/components/filterpanel/StopInput.js
+++ src/components/filterpanel/StopInput.js
@@ -19,22 +19,8 @@ const renderSuggestion = (suggestion) => (
 );
 
 const suggestionFitness = (inputValue) => (stop) => {
-  //const inputArr = !isNaN(inputValue)
-  //  ? inputValue.match(/[\d]/g).filter((char) => char !== " ")
-  //  : inputValue.toLowerCase().split();
-
-  //const stopIdArr = !isNaN(inputValue)
-  //  ? (stop.shortId + stop.stopId).match(/[\d]/g)
-  //  : stop.nameFi.toLowerCase().split();
-
-  //const fitnessScore = inputArr.reduce((score, val, index) => {
-  //  const foundIndex = stopIdArr.indexOf(val, index);
-  //  return score + (foundIndex - index);
-  //}, 0);
-
-  //return fitnessScore;
   if (stop.shortId.slice(2).startsWith(inputValue)) return 3;
-  if (stop.shortId.includes(inputValue)) return 2;
+  if (stop.shortId.toLowerCase().startsWith(inputValue)) return 2;
   if (
     stop.stopId.startsWith(inputValue) ||
     stop.nameFi.toLowerCase().startsWith(inputValue)
@@ -62,7 +48,7 @@ const getSuggestions = (stops = []) => (value = "") => {
     suggestionStops,
     [inputLength ? suggestionFitness(inputValue) : (x) => 0, "stopId"],
     ["desc", "asc"]
-  ).slice(0, 100);
+  ).slice(0, 50);
 };
 
 export default withStop(
