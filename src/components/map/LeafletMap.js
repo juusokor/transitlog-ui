55d29e4e0a542eededeaadeaae9ac5dff3a12567
Daniel Dunderfelt <daniel@developsuperpowers.com>
2018-09-03 14:58:46 +0300

    Switch to HSY map WIP. Projection doesn't work yet.

--- src/components/map/LeafletMap.js
+++ src/components/map/LeafletMap.js
@@ -1,11 +1,20 @@
 import React, {Component} from "react";
 import {Map, TileLayer, ZoomControl, Pane, LayersControl} from "react-leaflet";
-import {GoogleLayer} from "react-leaflet-google";
+import * as L from "leaflet";
+import "proj4leaflet";
 import "leaflet/dist/leaflet.css";
 import "./Map.css";
 import get from "lodash/get";
 
-const googleKey = "AIzaSyCD6EsHkznqImEhDQ4MvFGHya10gJdqqyI";
+const crs = new L.Proj.CRS(
+  "EPSG:3879",
+  "+proj=tmerc +lat_0=0 +lon_0=25 +k=1 +x_0=25500000 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs",
+  {
+    resolutions: [8192, 4096, 2048, 1024, 512, 256],
+    origin: [24747856.43, 6562789.7],
+    bounds: L.bounds(L.point(-16.1, 32.88), L.point(40.18, 84.17)),
+  }
+);
 
 export class LeafletMap extends Component {
   mapRef = React.createRef();
@@ -26,16 +35,17 @@ export class LeafletMap extends Component {
 
     return (
       <Map
+        crs={crs}
         ref={this.mapRef}
         center={center}
         zoom={zoom}
         bounds={bounds}
-        maxZoom={18} /* Google satellite layer doesn't handle more than 18 */
+        maxZoom={20}
         zoomControl={false}
         onViewportChanged={this.onViewportChange(onMapChanged)}
         onViewportChange={this.onViewportChange(onMapChange)}>
         <LayersControl position="topright">
-          <LayersControl.BaseLayer name="Digitransit" checked={true}>
+          <LayersControl.BaseLayer name="Digitransit">
             <TileLayer
               attribution={
                 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors '
@@ -45,11 +55,12 @@ export class LeafletMap extends Component {
               zoomOffset={-1}
             />
           </LayersControl.BaseLayer>
-          <LayersControl.BaseLayer name="Google Hybrid">
-            <GoogleLayer
-              googlekey={googleKey}
-              maptype="HYBRID"
-              libraries={["geometry", "places"]}
+          <LayersControl.BaseLayer name="Aerial map" checked={true}>
+            <TileLayer
+              attribution="LIJ/HSY"
+              tms={true}
+              tileSize={256}
+              url="https://partner-info.lij.hsl.fi/geoserver/gwc/service/tms/1.0.0/hsl:HSL_GK25_2500m_converted@EPSG:3879-HSL@jpeg/{z}/{x}/{y}.jpg"
             />
           </LayersControl.BaseLayer>
         </LayersControl>
