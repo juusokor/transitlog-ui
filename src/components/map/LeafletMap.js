c25332916f7c03f379465057fbd8e0bc7ed6f8d5
Daniel Dunderfelt <daniel@developsuperpowers.com>
2018-09-03 11:31:10 +0300

    Add satellite tile layer.

--- src/components/map/LeafletMap.js
+++ src/components/map/LeafletMap.js
@@ -1,8 +1,11 @@
 import React, {Component} from "react";
-import {Map, TileLayer, ZoomControl, Pane} from "react-leaflet";
+import {Map, TileLayer, ZoomControl, Pane, LayersControl} from "react-leaflet";
+import {ReactLeafletGoogle} from "../../lib/react-leaflet-google/ReactLeafletGoogle";
 import "leaflet/dist/leaflet.css";
 import "./Map.css";
 import get from "lodash/get";
+
+const googleKey = "AIzaSyCD6EsHkznqImEhDQ4MvFGHya10gJdqqyI";
 
 export class LeafletMap extends Component {
   mapRef = React.createRef();
@@ -27,22 +30,33 @@ export class LeafletMap extends Component {
         center={center}
         zoom={zoom}
         bounds={bounds}
-        maxZoom={20}
+        maxZoom={18} /* Google satellite layer doesn't handle more than 18 */
         zoomControl={false}
         onViewportChanged={this.onViewportChange(onMapChanged)}
         onViewportChange={this.onViewportChange(onMapChange)}>
+        <LayersControl position="topright">
+          <LayersControl.BaseLayer name="Digitransit" checked={true}>
+            <TileLayer
+              attribution={
+                'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors '
+              }
+              url="https://digitransit-prod-cdn-origin.azureedge.net/map/v1/hsl-map/{z}/{x}/{y}@2x.png"
+              tileSize={512}
+              zoomOffset={-1}
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name="Google Hybrid">
+            <ReactLeafletGoogle
+              googlekey={googleKey}
+              maptype="HYBRID"
+              libraries={["geometry", "places"]}
+            />
+          </LayersControl.BaseLayer>
+        </LayersControl>
         <Pane name="route-lines" style={{zIndex: 410}} />
         <Pane name="hfp-lines" style={{zIndex: 420}} />
         <Pane name="hfp-markers" style={{zIndex: 430}} />
         <Pane name="stops" style={{zIndex: 450}} />
-        <TileLayer
-          attribution={
-            'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors '
-          }
-          url="https://digitransit-prod-cdn-origin.azureedge.net/map/v1/hsl-map/{z}/{x}/{y}@2x.png"
-          tileSize={512}
-          zoomOffset={-1}
-        />
         <ZoomControl position="topright" />
         {children}
       </Map>
