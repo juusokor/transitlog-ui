c7ed0abe197c3f46979a9916be74d7c3235e041b
Daniel Dunderfelt <daniel@developsuperpowers.com>
2018-09-03 15:29:56 +0300

    Fix terminal stop to be red when journey was started too early.

--- src/components/map/StopMarker.js
+++ src/components/map/StopMarker.js
@@ -7,6 +7,7 @@ import TimingStopIcon from "../../icon-time1.svg";
 import get from "lodash/get";
 import reverse from "lodash/reverse";
 import diffDates from "date-fns/difference_in_seconds";
+import parse from "date-fns/parse";
 
 const stopColor = "#3388ff";
 const selectedStopColor = darken(0.2, stopColor);
@@ -24,25 +25,31 @@ export default ({
 }) => {
   const isTerminal = firstTerminal || lastTerminal;
 
+  let journeyStartedOnTime;
+
+  // Timing stops don't work properly until we have correct timetable info.
+  // TODO: Fix this up when we have.
+
+  if ((firstTerminal || stop.timingStopType) && hfp.length === 1) {
+    const date = time.format("YYYY-MM-DD");
+    const journeyStartHfp = get(reverse(hfp), `[0].journeys[0].depart`, "");
+
+    const startedDate = parse(journeyStartHfp.receivedAt);
+    const journeyStartDate = new Date(`${date}T${journeyStartHfp.journeyStartTime}`);
+
+    // Not "on time" if started 10 or more seconds too early.
+    journeyStartedOnTime = diffDates(journeyStartDate, startedDate) < 10;
+  }
+
   const timingStopIcon = icon({
     iconUrl: TimingStopIcon,
     iconSize: [30, 30],
     iconAnchor: [23, 25 / 2],
     popupAnchor: [3, -15],
-    className: "stop-marker timing-stop",
+    className: `stop-marker timing-stop ${
+      journeyStartedOnTime ? "on-time" : "early"
+    }`,
   });
-
-  let journeyStartedOnTime;
-
-  if (firstTerminal && hfp.length === 1) {
-    const date = time.format("YYYY-MM-DD");
-    const journeyStartHfp = get(reverse(hfp), `[0].journeys[0].depart`, "");
-
-    const startedDate = new Date(journeyStartHfp.receivedAt);
-    const journeyStartDate = new Date(`${date}T${journeyStartHfp.journeyStartTime}`);
-
-    journeyStartedOnTime = Math.abs(diffDates(startedDate, journeyStartDate)) <= 60;
-  }
 
   return React.createElement(
     stop.timingStopType ? Marker : CircleMarker,
