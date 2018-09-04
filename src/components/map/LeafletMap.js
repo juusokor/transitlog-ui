f40c572735aa4e9dea4250716dec44d532d07bc3
Daniel Dunderfelt <daniel@developsuperpowers.com>
2018-09-04 09:08:46 +0300

    Use MML/LUKE aerial map for now. Remove projection stuff.

--- src/components/map/LeafletMap.js
+++ src/components/map/LeafletMap.js
@@ -1,20 +1,8 @@
 import React, {Component} from "react";
 import {Map, TileLayer, ZoomControl, Pane, LayersControl} from "react-leaflet";
-import * as L from "leaflet";
-import "proj4leaflet";
 import "leaflet/dist/leaflet.css";
 import "./Map.css";
 import get from "lodash/get";
-
-const crs = new L.Proj.CRS(
-  "EPSG:3879",
-  "+proj=tmerc +lat_0=0 +lon_0=25 +k=1 +x_0=25500000 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs",
-  {
-    resolutions: [8192, 4096, 2048, 1024, 512, 256],
-    origin: [24747856.43, 6562789.7],
-    bounds: L.bounds(L.point(-16.1, 32.88), L.point(40.18, 84.17)),
-  }
-);
 
 export class LeafletMap extends Component {
   mapRef = React.createRef();
@@ -35,7 +23,6 @@ export class LeafletMap extends Component {
 
     return (
       <Map
-        crs={crs}
         ref={this.mapRef}
         center={center}
         zoom={zoom}
@@ -45,7 +32,7 @@ export class LeafletMap extends Component {
         onViewportChanged={this.onViewportChange(onMapChanged)}
         onViewportChange={this.onViewportChange(onMapChange)}>
         <LayersControl position="topright">
-          <LayersControl.BaseLayer name="Digitransit">
+          <LayersControl.BaseLayer name="Digitransit" checked={true}>
             <TileLayer
               attribution={
                 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors '
@@ -55,12 +42,10 @@ export class LeafletMap extends Component {
               zoomOffset={-1}
             />
           </LayersControl.BaseLayer>
-          <LayersControl.BaseLayer name="Aerial map" checked={true}>
+          <LayersControl.BaseLayer name="Aerial map">
             <TileLayer
-              attribution="LIJ/HSY"
-              tms={true}
-              tileSize={256}
-              url="https://partner-info.lij.hsl.fi/geoserver/gwc/service/tms/1.0.0/hsl:HSL_GK25_2500m_converted@EPSG:3879-HSL@jpeg/{z}/{x}/{y}.jpg"
+              attribution="MML/LUKE"
+              url="http://tiles.kartat.kapsi.fi/ortokuva/{z}/{x}/{y}.jpg"
             />
           </LayersControl.BaseLayer>
         </LayersControl>
