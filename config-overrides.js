1e41b7aa980562c297f5194bfac8d3d55d91efe7
Daniel Dunderfelt <daniel@developsuperpowers.com>
2018-10-05 08:20:46 +0300

    Enable react-hot-loader, update dependencies.

--- config-overrides.js
+++ config-overrides.js
@@ -3,10 +3,12 @@ const {
   addDecoratorsLegacy,
   disableEsLint,
   addBundleVisualizer,
+  addBabelPlugin,
 } = require("customize-cra");
 
 module.exports = override(
   addDecoratorsLegacy(),
   disableEsLint(),
+  addBabelPlugin("react-hot-loader/babel"),
   process.env.BUNDLE_VISUALIZE == 1 && addBundleVisualizer()
 );
