091bcaf9d728898a5e939cc8e7671d4169f96719
Henna Kalliokoski <henna.ruohonen@helsinki.fi>
2018-10-08 07:30:26 +0000

    stopquery

--- src/components/map/StopLayer.js
+++ src/components/map/StopLayer.js
@@ -42,6 +42,30 @@ const stopsByBboxQuery = gql`
   }
 `;
 
+const departuresByStopQuery = gql`
+query stopDepartures($stopId: String!, $routeId: String!, $dayType: String!, $dateBegin: Date!, $dateEnd: Date!) {
+    allDepartures(
+      condition: {
+        stopId: $stopId
+        dayType: $dayType
+        dateBegin: $dateBegin
+        dateEnd: $dateEnd
+      }
+      orderBy: DEPARTURE_ID_ASC
+    ) {
+      nodes {
+        stopId
+        routeId
+        departureId
+        hours
+        minutes
+        dateBegin
+        dateEnd
+        dayType
+      }
+    }
+  }`;
+
 const stopColor = "#3388ff";
 
 @inject(app("Filters"))
@@ -54,7 +78,7 @@ class StopLayer extends Component {
   };
 
   render() {
-    const {selectedStop, bounds, state} = this.props;
+    const {selectedStop, bounds, state, dateBegin, dateEnd} = this.props;
     const {date} = state;
 
     return (
@@ -63,9 +87,12 @@ class StopLayer extends Component {
           if (loading) return "Loading...";
           if (error) return "Error!";
           const stops = get(data, "stopsByBbox.nodes", []);
+          console.log(this.state)
           return (
+          
             <React.Fragment>
               {stops.map((stop) => (
+              
                 <CircleMarker
                   key={`stops_${stop.stopId}`}
                   pane="stops"
@@ -77,19 +104,32 @@ class StopLayer extends Component {
                   onPopupopen={() => this.setState({selectedStop: stop.stopId})}
                   onPopupclose={() => this.setState({selectedStop: null})}>
                   {this.state.selectedStop === stop.stopId ? (
+                  
                     <Popup>
+                    <Query query={departuresByStopQuery} variables={{...stop}}>
+                    {({loading, data, error}) => {
+                      if (loading) return "Loading...";
+                      if (error) return "Error!";
+                      const time = get(data, "allDepartures.nodes", []);
+                      console.log(this.state)
+                      return (
+                      
+                      <React.Fragment>
                       <h4>
                         {stop.nameFi}, {stop.shortId.replace(/ /g, "")} (
                         {stop.stopId})
                       </h4>
-                      {stop.routeSegmentsForDate.nodes.map((route) => (
-                        <button
-                          key={`route_${route.routeId}_${route.direction}`}
-                          className={"stop-route-list"}
-                          onClick={this.selectRoute(route)}>
-                          {route.routeId.substring(1).replace(/^0+/, "")}
-                        </button>
-                      ))}
+                        {stop.routeSegmentsForDate.nodes.map((route) => (
+                          <button
+                            key={`route_${route.routeId}_${route.direction}`}
+                            className={"stop-route-list"}
+                            onClick={this.selectRoute(route)}>
+                            {route.routeId.substring(1).replace(/^0+/, "")}
+                          </button>
+                        ))}
+                      </React.Fragment>
+                      )}}
+                      </Query>
                     </Popup>
                   ) : (
                     <Popup>Loading..</Popup>
