5fd158c8cee10a86846e2ad85ca2588b1228cf61
Daniel Dunderfelt <daniel@developsuperpowers.com>
2018-10-11 15:02:35 +0300

    Expandable filter section.

---
+++ src/components/filterbar/FilterSection.js
@@ -0,0 +1,83 @@
+import React, {Component} from "react";
+import {observer} from "mobx-react";
+import styled, {css} from "styled-components";
+
+const SectionVisible = styled.div`
+  height: 100%;
+  width: 100%;
+  padding: 0.75rem 1rem 1rem;
+  border-right: 1px solid var(--alt-grey);
+  position: relative;
+  z-index: 20;
+`;
+
+const SectionExpandable = styled.div`
+  width: calc(100% + 1px);
+  opacity: 0;
+  max-height: 0;
+  overflow: hidden;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  z-index: 20;
+  transform: translateY(100%);
+  background: var(--lightest-grey);
+  border-right: 1px solid var(--alt-grey);
+  border-left: 1px solid var(--alt-grey);
+  margin-left: -1px;
+  padding: 0.75rem 1rem 1rem;
+  border-bottom: 1px solid var(--alt-grey);
+
+  ${({expanded = false}) =>
+    expanded
+      ? css`
+          opacity: 1;
+          max-height: none;
+        `
+      : ""};
+`;
+
+const FilterSectionWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  position: relative;
+  z-index: 20;
+`;
+
+@observer
+class FilterSection extends Component {
+  state = {
+    expanded: false,
+  };
+
+  onTrigger = (state) => (e) => {
+    setTimeout(() => {
+      if (this.state.expanded !== state) {
+        this.setState({
+          expanded: state,
+        });
+      }
+    }, 30);
+  };
+
+  render() {
+    const {className, children, expandable} = this.props;
+    const {expanded} = this.state;
+
+    return (
+      <FilterSectionWrapper
+        className={className}
+        onMouseEnter={this.onTrigger(true)}
+        onMouseLeave={this.onTrigger(false)}>
+        {expandable && (
+          <SectionExpandable expanded={expanded}>{expandable}</SectionExpandable>
+        )}
+        <SectionVisible>{children}</SectionVisible>
+      </FilterSectionWrapper>
+    );
+  }
+}
+
+export default FilterSection;
