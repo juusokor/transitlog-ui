406f52aa07ead4d66c5ae18ad62c2b283df79c11
Henna Kalliokoski <henna.ruohonen@helsinki.fi>
2018-10-13 22:09:36 +0000

    delay times for timetables

--- src/components/map/StopTimetable.js
+++ src/components/map/StopTimetable.js
@@ -8,11 +8,12 @@ import {app} from "mobx-app";
 import withRoute from "../../hoc/withRoute";
 import getDay from "date-fns/get_day";
 import styled from "styled-components";
+import {hfpClient} from "../../api";
 
 const departuresQuery = gql`
   query routeDepartures($dayType: String, $stopId: String) {
     allDepartures(
-      orderBy: DEPARTURE_ID_ASC
+      orderBy: [HOURS_ASC, MINUTES_ASC, DEPARTURE_ID_ASC]
       condition: {stopId: $stopId, dayType: $dayType}
     ) {
       nodes {
@@ -23,7 +24,64 @@ const departuresQuery = gql`
         dateBegin
         dateEnd
         routeId
+        direction
+        departureId
       }
+    }
+  }
+`;
+
+const departureStartTimeQuery = gql`
+  query departureStartTime(
+    $routeId: String
+    $direction: String
+    $dateBegin: Date
+    $dateEnd: Date
+    $departureId: Int
+    $dayType: String
+  ) {
+    allDepartures(
+      first: 1
+      orderBy: [HOURS_ASC, MINUTES_ASC]
+      condition: {
+        routeId: $routeId
+        direction: $direction
+        dateBegin: $dateBegin
+        dateEnd: $dateEnd
+        departureId: $departureId
+        dayType: $dayType
+      }
+    ) {
+      nodes {
+        hours
+        minutes
+      }
+    }
+  }
+`;
+
+const stopDelayQuery = gql`
+  query stopDelay(
+    $routeId: String!
+    $date: date!
+    $direction: smallint!
+    $stopId: String!
+    $startTime: time!
+  ) {
+    vehicles(
+      limit: 1
+      order_by: received_at_desc
+      where: {
+        _and: {
+          route_id: {_eq: $routeId}
+          dir: {_eq: $direction}
+          oday: {_eq: $date}
+          journey_start_time: {_eq: $startTime}
+          next_stop_id: {_eq: $stopId}
+        }
+      }
+    ) {
+      dl
     }
   }
 `;
@@ -47,6 +105,60 @@ const TimetableTime = styled.div`
   padding: 0.2em;
 `;
 
+const parseLineNumber = (lineId) =>
+  // Remove 1st number, which represents the city
+  // Remove all zeros from the beginning
+  lineId.substring(1).replace(/^0+/, "");
+
+const StopDelay = ({
+  routeId,
+  direction,
+  dateBegin,
+  dateEnd,
+  departureId,
+  dayType,
+  date,
+  stopId,
+}) => (
+  <Query
+    query={departureStartTimeQuery}
+    variables={{
+      routeId,
+      direction,
+      dateBegin,
+      dateEnd,
+      departureId,
+      dayType,
+    }}>
+    {({loading, data, error}) => {
+      if (loading) return "?";
+      if (error) return "Error!";
+      const {hours, minutes} = get(data, "allDepartures.nodes", [])[0];
+      const startTime = `${("0" + hours).slice(-2)}:${("0" + minutes).slice(-2)}:00`;
+      return (
+        <Query
+          client={hfpClient}
+          variables={{routeId, date, direction, stopId, startTime}}
+          query={stopDelayQuery}>
+          {({loading, data, error}) => {
+            if (loading) return "?";
+            if (error) return "Error!";
+
+            const vehicles = get(data, "vehicles", []);
+            if (vehicles.length === 0) return "(eiolee)";
+            const {dl} = vehicles[0];
+            if (dl === 0) return "+-0:00";
+            const sign = dl < 0 ? "+" : "-";
+            const seconds = Math.abs(dl) % 60;
+            const minutes = Math.floor(Math.abs(dl) / 60);
+            return `${sign}${minutes}:${("0" + seconds).slice(-2)}`;
+          }}
+        </Query>
+      );
+    }}
+  </Query>
+);
+
 @inject(app("Filters"))
 @observer
 class StopTimetable extends Component {
@@ -62,14 +174,12 @@ class StopTimetable extends Component {
         {({loading, data, error}) => {
           if (loading) return "Loading...";
           if (error) return "Error!";
-          console.log(error, data);
           const timetable = get(data, "allDepartures.nodes", []);
           const filteredTimetable = timetable.filter(
             ({dateBegin, dateEnd}) =>
               this.props.date >= dateBegin && this.props.date <= dateEnd
           );
           const byHour = groupBy(filteredTimetable, "hours");
-          console.log(byHour);
           return (
             <TimetableGrid>
               {Object.entries(byHour).map(([hour, times]) => (
@@ -79,7 +189,8 @@ class StopTimetable extends Component {
                     {times.map((time) => (
                       <TimetableTime>
                         {" "}
-                        {time.minutes}/{time.routeId}{" "}
+                        {time.minutes}/{parseLineNumber(time.routeId)}
+                        <StopDelay {...time} date={this.props.date} />{" "}
                       </TimetableTime>
                     ))}
                   </TimetableTimes>
