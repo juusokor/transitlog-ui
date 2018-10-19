8e55ea422f6acf2ad092b6cb510c61518d1963a7
Daniel Dunderfelt <daniel@developsuperpowers.com>
2018-10-19 09:33:20 +0300

    Refactor Departuresquery vars.

--- src/queries/DeparturesQuery.js
+++ src/queries/DeparturesQuery.js
@@ -7,9 +7,12 @@ import isWithinRange from "date-fns/is_within_range";
 import gql from "graphql-tag";
 import getDay from "date-fns/get_day";
 import get from "lodash/get";
+import compact from "lodash/compact";
+import reduce from "lodash/reduce";
+import pick from "lodash/pick";
 
 const departuresQuery = gql`
-  query routeDepartures(
+  query allDepartures(
     $routeId: String
     $direction: String
     $dayType: String
@@ -61,67 +64,64 @@ class DeparturesQuery extends Component {
     stop: PropTypes.oneOfType([
       PropTypes.string,
       PropTypes.shape({
-        stopId: PropTypes.string.isRequired,
+        stopId: PropTypes.string,
       }),
     ]),
     departureId: PropTypes.number,
     dateBegin: PropTypes.string,
     dateEnd: PropTypes.string,
+    limit: PropTypes.number,
   };
 
   static defaultProps = {
     route: {
-      routeId: "",
-      direction: "",
+      routeId: null,
+      direction: null,
     },
     stop: {
-      stopId: "",
+      stopId: null,
     },
   };
 
   render() {
     const {
-      route = {routeId: "", direction: ""},
-      stop = {stopId: ""},
+      route,
+      stop,
       date,
       dateBegin,
       dateEnd,
       departureId,
+      limit,
       children,
     } = this.props;
+
     const queryDayType = dayTypes[getDay(date)];
 
-    const {routeId = "", direction = "", originstopId = ""} = route;
+    const {routeId, direction, originstopId} = route;
     const stopId = get(stop, "stopId", stop);
 
-    let queryVars = {};
-
-    if (!routeId && stopId && typeof stopId === "string") {
-      // If we don't have a route, but we do have a stop, query for the stop departures.
-      queryVars = {
-        stopId,
+    let queryVars = reduce(
+      {
         dayType: queryDayType,
-      };
-    } else if (!departureId && routeId && direction && originstopId) {
-      // If we have a route with all required ingredients, query for the planned route journeys.
-      queryVars = {
-        routeId,
-        direction,
-        dayType: queryDayType,
-        stopId: originstopId,
-      };
-    } else if (departureId && dateBegin && dateEnd && routeId && direction) {
-      // In this case we want the start time of a journey when we have a departure from a stop.
-      queryVars = {
+        stopId: originstopId ? originstopId : stopId,
         routeId,
         direction,
         departureId,
         dateBegin,
         dateEnd,
-        dayType: queryDayType,
-        limit: 1,
-      };
-    } else {
+        limit,
+      },
+      (presentVars, value, key) => {
+        if (value) {
+          presentVars[key] = value;
+        }
+
+        return presentVars;
+      },
+      {}
+    );
+
+    if (Object.keys(queryVars).length < 2) {
       // If we don't have the required info, return an empty array to the render function.
       return children({departures: [], loading: false, error: null});
     }
