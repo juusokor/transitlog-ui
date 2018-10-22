c11c757a59c1f1658ee9f6983d26d4a7d205cfa4
Daniel Dunderfelt <daniel@developsuperpowers.com>
2018-10-22 07:21:21 +0300

    Use react-async to handle promises in the cache component.

--- src/hoc/withHfpData.js
+++ src/hoc/withHfpData.js
@@ -3,11 +3,9 @@ import {app} from "mobx-app";
 import React from "react";
 import get from "lodash/get";
 import {createFetchKey} from "../helpers/hfpCache";
-import {fromPromise} from "mobx-utils";
 import withRoute from "./withRoute";
 import {fetchHfp} from "../helpers/hfpQueryManager";
-
-const emptyCachePromise = () => fromPromise.resolve([]);
+import Async from "react-async";
 
 // To use while the promise is loading
 let previouslyResolvedPositions = [];
@@ -18,42 +16,29 @@ export default (Component) => {
   @observer
   class WithHfpData extends React.Component {
     currentFetchKey = false;
-    cachePromise = emptyCachePromise();
 
     // Creates a promise for awaiting the hfp result from the API or the cache.
     // React can't render async yet, so some mechanism to update the view when
     // an async result comes back is required. Remember that this runs *per instance*,
     // so the vast majority of work should be done in the `fetchHfp` method that runs
     // once for all instances when required.
-    updateCachePromise = () => {
-      const {
-        route,
-        state: {date, time, selectedJourney},
-      } = this.props;
-
-      if (this.cachePromise.state === "pending") {
-        return;
-      }
-
-      const useTime = get(selectedJourney, "journey_start_time", time);
-      const fetchKey = createFetchKey(route, date, useTime);
-      let setPromise;
+    updateCachePromise = async ({route, date, time}) => {
+      const fetchKey = createFetchKey(route, date, time);
+      let returnVal = [];
 
       // If we have a valid cacheKey (ie there is a route selected), and the key is
       // currently not in use, update the cache promise to fetch the current route.
       if (fetchKey && fetchKey !== this.currentFetchKey) {
-        setPromise = fromPromise(fetchHfp(route, date, useTime));
-      } else if (fetchKey !== this.currentFetchKey) {
-        setPromise = emptyCachePromise();
+        returnVal = await fetchHfp(route, date, time);
       }
 
-      // Always update the promise if the current cache key doesn't match the new one.
-      // This allows for empty cache promises to be set, even if the above condition doesn't run.
+      // Always update the cache key.
       // This is needed to update the view when the route changes or filters reset.
       if (fetchKey !== this.currentFetchKey) {
         this.currentFetchKey = fetchKey;
-        this.cachePromise = setPromise;
       }
+
+      return returnVal;
     };
 
     getComponent = (positions, loading) => (
@@ -66,20 +51,31 @@ export default (Component) => {
     );
 
     render() {
-      this.updateCachePromise();
+      const {
+        route,
+        state: {date, time, selectedJourney},
+      } = this.props;
 
-      // Use the mobxified promise
-      return this.cachePromise.case({
-        pending: () => this.getComponent(previouslyResolvedPositions, true),
-        rejected: (error) => {
-          console.error(error);
-          return this.getComponent(previouslyResolvedPositions, false);
-        },
-        fulfilled: (positions) => {
-          previouslyResolvedPositions = positions;
-          return this.getComponent(positions, false);
-        },
-      });
+      const fetchKey = createFetchKey(route, date, time);
+      const useTime = get(selectedJourney, "journey_start_time", time);
+
+      return (
+        <Async
+          watch={fetchKey}
+          promiseFn={this.updateCachePromise}
+          time={useTime}
+          date={date}
+          route={route}>
+          {({data, error, loading}) => {
+            if (!data || data.length === 0) {
+              return this.getComponent(previouslyResolvedPositions, loading);
+            }
+
+            previouslyResolvedPositions = data;
+            return this.getComponent(data, false);
+          }}
+        </Async>
+      );
     }
   }
 
