766b8991faeb00e1f41b389f092a0993b05910f2
Daniel Dunderfelt <daniel@developsuperpowers.com>
2018-12-20 11:18:43 +0200

    Equipment query by vehicle ID.

--- src/components/sidepanel/journeyDetails/Equipment.js
+++ src/components/sidepanel/journeyDetails/Equipment.js
@@ -1,12 +1,47 @@
 import React from "react";
 import gql from "graphql-tag";
 import styled from "styled-components";
+import {Query} from "react-apollo";
+import get from "lodash/get";
+import pick from "lodash/pick";
 
-const EquipmentWrapper = styled.div``;
+const EquipmentWrapper = styled.span``;
 
-// const vehicleTypeQuery = gql``;
+const vehicleTypeQuery = gql`
+  query vehicleInfo($vehicleId: String) {
+    allEquipment(condition: {vehicleId: $vehicleId}) {
+      nodes {
+        vehicleId
+        registryNr
+        age
+        type
+        multiAxle
+        exteriorColor
+        class
+      }
+    }
+  }
+`;
 
 export default ({journey}) => {
   const {unique_vehicle_id} = journey;
-  return <EquipmentWrapper>{unique_vehicle_id}</EquipmentWrapper>;
+  const vehicleId = unique_vehicle_id.split("/")[0];
+
+  return (
+    <Query query={vehicleTypeQuery} variables={{vehicleId}}>
+      {({data, loading, error}) => {
+        if (loading || error) return "...";
+
+        const vehicle = get(data, "allEquipment.nodes[0]", {});
+
+        return (
+          <EquipmentWrapper>
+            {Object.values(
+              pick(vehicle, "type", "multiAxle", "exteriorColor", "class")
+            ).join(", ")}
+          </EquipmentWrapper>
+        );
+      }}
+    </Query>
+  );
 };
