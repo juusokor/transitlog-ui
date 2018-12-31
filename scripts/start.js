56216e47a19a7c695b0c745a77f4015b26dabc99
Daniel Dunderfelt <daniel@developsuperpowers.com>
2018-12-31 07:55:43 +0200

    Disable automatic browser open.

--- scripts/start.js
+++ scripts/start.js
@@ -93,7 +93,8 @@ checkBrowsers(paths.appPath, isInteractive)
         clearConsole();
       }
       console.log(chalk.cyan("Starting the development server...\n"));
-      openBrowser(urls.localUrlForBrowser);
+      // this is annoying IMO
+      // openBrowser(urls.localUrlForBrowser);
     });
 
     ["SIGINT", "SIGTERM"].forEach(function(sig) {
