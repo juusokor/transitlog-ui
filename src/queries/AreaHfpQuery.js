c861956da5bf623fd844ce728e84b9e21a9d524c
Daniel Dunderfelt <daniel@developsuperpowers.com>
2019-01-28 10:33:30 +0200

    Set a fetchpolicy on area hfp query to make it clear the loading status correctly.

--- src/queries/AreaHfpQuery.js
+++ src/queries/AreaHfpQuery.js
@@ -83,6 +83,7 @@ class AreaHfpQuery extends Component {
 
     return (
       <Query
+        fetchPolicy="cache-and-network"
         partialRefetch={true}
         skip={skip}
         variables={{date, minTime, maxTime, minLat, maxLat, minLong, maxLong}}
