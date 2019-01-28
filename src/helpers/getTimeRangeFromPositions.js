bb850a0771e73631ff7e2747d47ea583973552c9
Daniel Dunderfelt <daniel@developsuperpowers.com>
2019-01-28 14:53:05 +0200

    Add tests for timerange from positions helper.

--- src/helpers/getTimeRangeFromPositions.js
+++ src/helpers/getTimeRangeFromPositions.js
@@ -6,15 +6,22 @@ export const dateToSeconds = (date, operationDay) => {
   return date.diff(operationDay, "seconds");
 };
 
+/*
+  Returns a range in seconds, relative to the operation day, for the provided array of hfp events.
+  The min and max values are seconds passed since the start of the operation day.
+ */
+
 export function getTimeRangeFromPositions(positions) {
   if (positions.length === 0) {
     return null;
   }
 
+  // The day when the journey is scheduled. This is the base that the seconds are relative to.
   const operationDay = moment
     .tz(get(positions, "[0].oday"), "Europe/Helsinki")
     .startOf("day");
 
+  // Min and max moments for the position range
   const minMoment = moment.tz(get(positions, "[0].received_at"), "Europe/Helsinki");
   const maxMoment = moment.tz(
     get(last(positions), "received_at"),
--- src/helpers/inflate.js
+++ src/helpers/inflate.js
@@ -1,7 +1,7 @@
 import set from "lodash/set";
 
 // Given an onject with keys like "path.into.nested.object",
-// returns a new nested object that mirros the paths from the keys.
+// returns a new nested object that mirrors the paths from the keys.
 
 export function inflate(obj) {
   const inflatedObj = {};
