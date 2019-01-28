299670d565e7fe499fc55129424059df2120d41e
Daniel Dunderfelt <daniel@developsuperpowers.com>
2019-01-28 07:49:50 +0200

    Remove unused timerange function.

--- src/helpers/time.js
+++ src/helpers/time.js
@@ -79,34 +79,3 @@ export function departureTime(departure) {
   const hour = isNextDay ? hours + 24 : hours;
   return getTimeString(hour, minutes);
 }
-
-export function getTimeRange(date) {
-  const queryMoment = date.seconds(0);
-
-  const quarter = Math.round((queryMoment.minutes() / 60) * 100);
-  let minMinutes;
-  let maxMinutes;
-  let maxAddHours = 0;
-
-  if (quarter <= 25) {
-    minMinutes = 0;
-    maxMinutes = 15;
-  } else if (quarter <= 50) {
-    minMinutes = 15;
-    maxMinutes = 30;
-  } else if (quarter <= 75) {
-    minMinutes = 30;
-    maxMinutes = 45;
-  } else {
-    minMinutes = 45;
-    maxMinutes = 0;
-    maxAddHours = 1;
-  }
-
-  const min = moment(queryMoment).minutes(minMinutes);
-  const max = moment(queryMoment)
-    .minutes(maxMinutes)
-    .add(maxAddHours, "hours");
-
-  return {max, min};
-}
