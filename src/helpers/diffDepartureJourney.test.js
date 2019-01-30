e65d8adb1a6ad4905460467f02f91e85524d532c
Daniel Dunderfelt <daniel@developsuperpowers.com>
2019-01-30 08:29:37 +0200

    Add 24h+ test for diffDepartureJourney.

--- src/helpers/diffDepartureJourney.test.js
+++ src/helpers/diffDepartureJourney.test.js
@@ -17,10 +17,38 @@ describe("diffDepartureJourney", () => {
       received_at: "2019-01-27T16:05:30.000Z", // UTC timestamp 1:30 minutes after scheduled start
     };
 
-    const diff = diffDepartureJourney(event, departure, "2019-01-27");
+    const diff = diffDepartureJourney(event, departure, date);
 
-    const expectPlannedMoment = getMomentFromDateTime("2019-01-27", "16:04:00");
-    const expectObservedMoment = getMomentFromDateTime("2019-01-27", "16:05:30");
+    const expectPlannedMoment = getMomentFromDateTime(date, "16:04:00");
+    const expectObservedMoment = getMomentFromDateTime(date, "16:05:30");
+
+    expect(diff.diff).toBe(90);
+    expect(diff.minutes).toBe(1);
+    expect(diff.seconds).toBe(30);
+    expect(diff.sign).toBe("+");
+    expect(diff.observedMoment.valueOf()).toBe(expectObservedMoment.valueOf());
+    expect(diff.plannedMoment.valueOf()).toBe(expectPlannedMoment.valueOf());
+  });
+
+  test("diffDepartureJourney works with 24h+ times", () => {
+    const date = "2019-01-27";
+
+    const departure = {
+      hours: 2,
+      minutes: 10,
+      isNextDay: true,
+    };
+
+    const event = {
+      journey_start_time: "26:10:00",
+      oday: date,
+      received_at: "2019-01-28T02:11:30.000Z", // UTC timestamp 1:30 minutes after scheduled start
+    };
+
+    const diff = diffDepartureJourney(event, departure, date);
+
+    const expectPlannedMoment = getMomentFromDateTime(date, "26:10:00");
+    const expectObservedMoment = getMomentFromDateTime(date, "26:11:30");
 
     expect(diff.diff).toBe(90);
     expect(diff.minutes).toBe(1);
