e119f90e5851e84174bc44dd01988ef0a7b9d020
Daniel Dunderfelt <daniel@developsuperpowers.com>
2019-02-11 10:14:22 +0200

    Add loading spinner to vehicle and stop input.

--- src/components/filterbar/StopSettings.js
+++ src/components/filterbar/StopSettings.js
@@ -6,34 +6,39 @@ import {inject, observer} from "mobx-react";
 import {app} from "mobx-app";
 import AllStopsQuery from "../../queries/AllStopsQuery";
 import StopInput from "./StopInput";
-import StopsByRouteQuery from "../../queries/StopsByRouteQuery";
+import Loading from "../Loading";
+import styled from "styled-components";
+
+const LoadingSpinner = styled(Loading)`
+  margin: 0.5rem 0.5rem 0.5rem 1rem;
+`;
 
 @inject(app("Filters"))
 @observer
 class StopSettings extends React.Component {
   render() {
     const {Filters, state} = this.props;
-    const {stop, route} = state;
+    const {stop} = state;
 
     return (
       <>
         <ControlGroup>
           <Input label={text("filterpanel.filter_by_stop")} animatedLabel={false}>
-            {!route.routeId || !route.dateBegin ? (
-              <AllStopsQuery key="all_stops">
-                {({stops}) => (
-                  <StopInput onSelect={Filters.setStop} stop={stop} stops={stops} />
-                )}
-              </AllStopsQuery>
-            ) : (
-              <StopsByRouteQuery
-                key={`stop_input_by_route_${route.routeId}`}
-                route={route}>
-                {({stops}) => (
-                  <StopInput onSelect={Filters.setStop} stop={stop} stops={stops} />
-                )}
-              </StopsByRouteQuery>
-            )}
+            <AllStopsQuery key="all_stops">
+              {({stops, loading}) => {
+                if (loading) {
+                  return <LoadingSpinner inline={true} />;
+                }
+                return (
+                  <StopInput
+                    onSelect={Filters.setStop}
+                    stop={stop}
+                    stops={stops}
+                    loading={loading}
+                  />
+                );
+              }}
+            </AllStopsQuery>
           </Input>
         </ControlGroup>
         {!!stop && (
