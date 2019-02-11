4c1f99060d9ff2c85035d933a1d3652dc6000eeb
Daniel Dunderfelt <daniel@developsuperpowers.com>
2019-02-11 12:54:59 +0200

    Adjust journey sidebar layout and design.

--- src/components/sidepanel/journeyDetails/JourneyInfo.js
+++ src/components/sidepanel/journeyDetails/JourneyInfo.js
@@ -19,12 +19,12 @@ const JourneyInfoRow = styled.div`
   justify-content: flex-start;
   width: 100%;
   padding: 0.5rem 1rem;
-  background: var(--lightest-grey);
+  background: transparent;
   font-size: 1rem;
   font-family: inherit;
 
   &:nth-child(even) {
-    background: rgba(0, 0, 0, 0.0075);
+    background: rgba(0, 0, 0, 0.03);
   }
 `;
 
@@ -43,8 +43,8 @@ const Line = styled.div`
 `;
 
 const LineHeading = styled.span`
-  color: var(--light-grey);
-  font-size: 1rem;
+  color: #888888;
+  font-size: 0.9rem;
   flex-wrap: nowrap;
   white-space: nowrap;
 `;
@@ -96,86 +96,6 @@ export default ({
     <JourneyInfo>
       <JourneyInfoRow>
         <Line>
-          <LineHeading>Operator</LineHeading>
-          <Values small>
-            <span>{operatorName}</span>
-          </Values>
-        </Line>
-        {observedOperatorName !== operatorName && (
-          <Line>
-            <LineHeading>Subcontractor</LineHeading>
-            <Values small>
-              <ObservedValue>{observedOperatorName}</ObservedValue>
-            </Values>
-          </Line>
-        )}
-      </JourneyInfoRow>
-      <Equipment journey={journey} departure={departure}>
-        {({equipment = [], vehicle = null}) => (
-          <>
-            {!!vehicle && (
-              <>
-                <JourneyInfoRow>
-                  <Line>
-                    <LineHeading>
-                      <Text>vehicle.registry_nr</Text>
-                    </LineHeading>
-                    <Values>
-                      <span>{vehicle.registryNr}</span>
-                    </Values>
-                  </Line>
-                </JourneyInfoRow>
-                <JourneyInfoRow>
-                  <Line>
-                    <LineHeading>
-                      <Text>vehicle.age</Text>
-                    </LineHeading>
-                    <Values>
-                      <span>{vehicle.age}</span>
-                    </Values>
-                  </Line>
-                </JourneyInfoRow>
-              </>
-            )}
-            {equipment.length && (
-              <JourneyInfoRow>
-                <Line>
-                  <LineHeading>
-                    <Text>journey.requested_equipment</Text>
-                  </LineHeading>
-                  <Values>
-                    <span>
-                      {equipmentType
-                        ? equipmentType
-                        : equipmentCode
-                        ? equipmentCode
-                        : text("general.no_type")}
-                      {get(departure, "trunkColorRequired", 0) === 1 &&
-                        ", HSL-orans"}
-                    </span>
-                  </Values>
-                </Line>
-                <Line right>
-                  <Values>
-                    {equipment.map((prop) => (
-                      <ObservedValue
-                        key={`equipment_prop_${prop.name}`}
-                        backgroundColor={prop.color}
-                        color={
-                          prop.required !== false ? "white" : "var(--dark-grey)"
-                        }>
-                        {prop.observed}
-                      </ObservedValue>
-                    ))}
-                  </Values>
-                </Line>
-              </JourneyInfoRow>
-            )}
-          </>
-        )}
-      </Equipment>
-      <JourneyInfoRow>
-        <Line>
           <LineHeading>
             <Text>journey.terminal_time</Text>
           </LineHeading>
@@ -225,6 +145,104 @@ export default ({
           </Values>
         </Line>
       </JourneyInfoRow>
+      <JourneyInfoRow>
+        <Line>
+          <LineHeading>
+            <Text>vehicle.identifier</Text>
+          </LineHeading>
+          <Values>
+            <span>{journey.unique_vehicle_id}</span>
+          </Values>
+        </Line>
+      </JourneyInfoRow>
+      <JourneyInfoRow>
+        <Line>
+          <LineHeading>
+            <Text>vehicle.operator</Text>
+          </LineHeading>
+          <Values small>
+            <span>{operatorName}</span>
+          </Values>
+        </Line>
+        {observedOperatorName !== operatorName && (
+          <Line>
+            <LineHeading>
+              <Text>vehicle.subcontractor</Text>
+            </LineHeading>
+            <Values small>
+              <ObservedValue>{observedOperatorName}</ObservedValue>
+            </Values>
+          </Line>
+        )}
+      </JourneyInfoRow>
+      <Equipment journey={journey} departure={departure}>
+        {({equipment = [], vehicle = null}) => (
+          <>
+            {!!vehicle && (
+              <>
+                <JourneyInfoRow>
+                  <Line>
+                    <LineHeading>
+                      <Text>vehicle.registry_nr</Text>
+                    </LineHeading>
+                    <Values>
+                      <span>{vehicle.registryNr}</span>
+                    </Values>
+                  </Line>
+                </JourneyInfoRow>
+                <JourneyInfoRow>
+                  <Line>
+                    <LineHeading>
+                      <Text>vehicle.age</Text>
+                    </LineHeading>
+                    <Values>
+                      <span>{vehicle.age}</span>
+                      &nbsp;
+                      <Text>
+                        {vehicle.age < 2 ? "general.year" : "general.year.plural"}
+                      </Text>
+                    </Values>
+                  </Line>
+                </JourneyInfoRow>
+              </>
+            )}
+            {equipment.length && (
+              <JourneyInfoRow>
+                <Line>
+                  <LineHeading>
+                    <Text>journey.requested_equipment</Text>
+                  </LineHeading>
+                  <Values>
+                    <span>
+                      {equipmentType
+                        ? equipmentType
+                        : equipmentCode
+                        ? equipmentCode
+                        : text("general.no_type")}
+                      {get(departure, "trunkColorRequired", 0) === 1 &&
+                        ", HSL-orans"}
+                    </span>
+                  </Values>
+                </Line>
+                <Line right>
+                  <Values>
+                    {equipment.map((prop) => (
+                      <ObservedValue
+                        key={`equipment_prop_${prop.name}`}
+                        backgroundColor={prop.color}
+                        color={
+                          prop.required !== false ? "white" : "var(--dark-grey)"
+                        }>
+                        {prop.observed}
+                      </ObservedValue>
+                    ))}
+                  </Values>
+                </Line>
+              </JourneyInfoRow>
+            )}
+          </>
+        )}
+      </Equipment>
     </JourneyInfo>
   );
 };
