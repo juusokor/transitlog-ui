5fd31565e5c591ea8ed386cf76685115897a34dc
Daniel Dunderfelt <daniel@developsuperpowers.com>
2019-02-18 11:01:29 +0200

    Add intersection observer to check when the rect is in view or not.

--- src/helpers/Help.js
+++ src/helpers/Help.js
@@ -1,10 +1,12 @@
-import React, {useRef, useEffect, useMemo} from "react";
+import React, {useRef, useEffect, useMemo, useState, useCallback} from "react";
 import pick from "lodash/pick";
 import {registerTooltip} from "./Tooltip";
 import {helpText as translateHelpText} from "./text";
 
 const Help = ({children, rectRef = null, helpText = "This is Help"}) => {
   let hoverRef = useRef(null);
+  const observerRef = useRef(null);
+  const [rect, setRect] = useState(null);
 
   if (rectRef) {
     hoverRef = rectRef;
@@ -17,23 +19,43 @@ const Help = ({children, rectRef = null, helpText = "This is Help"}) => {
     return React.cloneElement(onlyChild, {ref: hoverRef});
   }, [children]);
 
-  useEffect(() => {
-    if (
-      hoverRef.current &&
-      typeof hoverRef.current.getBoundingClientRect === "function" &&
-      translatedText
-    ) {
-      const rect = pick(
-        hoverRef.current.getBoundingClientRect(),
-        "top",
-        "left",
-        "right",
-        "bottom"
-      );
+  const intersectionCallback = useCallback(([entry]) => {
+    const currentRect = pick(
+      entry.boundingClientRect,
+      "top",
+      "left",
+      "right",
+      "bottom"
+    );
 
+    console.log(currentRect);
+    setRect(currentRect);
+  }, []);
+
+  useEffect(() => {
+    if (hoverRef.current && hoverRef.current instanceof Element) {
+      if (!observerRef.current) {
+        observerRef.current = new IntersectionObserver(intersectionCallback, {
+          root: null,
+        });
+      }
+
+      observerRef.current.observe(hoverRef.current);
+    }
+
+    return () => {
+      if (observerRef.current) {
+        observerRef.current.disconnect();
+        observerRef.current = null;
+      }
+    };
+  }, [hoverRef.current]);
+
+  useEffect(() => {
+    if (rect && translatedText) {
       return registerTooltip(rect, translatedText);
     }
-  }, [hoverRef.current, translatedText]);
+  }, [rect, translatedText]);
 
   return child;
 };
