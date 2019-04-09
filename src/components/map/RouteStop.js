28451b8fd80fceefa9e74c0abe6c28fdf88954a7
Daniel Dunderfelt <daniel@developsuperpowers.com>
2019-04-09 09:09:55 +0300

    Fix delay color for timing stops.

--- src/components/map/RouteStop.js
+++ src/components/map/RouteStop.js
@@ -105,7 +105,6 @@ class RouteStop extends React.Component {
 
     const mode = getPriorityMode(get(stop, "modes", []));
     let color = getModeColor(mode);
-    let delayType = "none";
 
     if (
       !selectedJourney ||
@@ -116,7 +115,7 @@ class RouteStop extends React.Component {
       return (
         <RouteStopMarker
           key={`route_stop_marker_${stop.stopId}`}
-          delayType={delayType}
+          delayType="none"
           color={color}
           isTerminal={isTerminal}
           stop={stop}
@@ -361,7 +360,7 @@ class RouteStop extends React.Component {
       <RouteStopMarker
         key={`journey_stop_marker_${stop.stopId}`}
         doorDidOpen={doorDidOpen}
-        delayType={delayType}
+        delayType={departureDelayType}
         color={color}
         isTerminal={isTerminal}
         stop={stop}
