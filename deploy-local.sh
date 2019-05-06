cbe599fa3d3ba7918bd9b8289374520791e3591e
Daniel Dunderfelt <daniel@developsuperpowers.com>
2019-05-06 12:09:02 +0300

    Remove extra push from deploy script.

--- deploy-local.sh
+++ deploy-local.sh
@@ -6,6 +6,5 @@ DOCKER_IMAGE=${ORG}/transitlog-ui:latest
 DOCKER_IMAGE_LATEST=${ORG}/transitlog-ui:latest
 
 docker build -t ${DOCKER_IMAGE} .
-docker push ${DOCKER_IMAGE}
 docker tag ${DOCKER_IMAGE} ${DOCKER_IMAGE_LATEST}
 docker push ${DOCKER_IMAGE_LATEST}
