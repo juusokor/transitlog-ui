c2d568ef7e4bb16371578dc4b716d3861183a71a
Daniel Dunderfelt <daniel@developsuperpowers.com>
2019-05-17 17:06:06 +0300

    Departures cancellations WIP.

--- src/components/sidepanel/TimetableDeparture.js
+++ src/components/sidepanel/TimetableDeparture.js
@@ -12,7 +12,7 @@ import {
   PlainSlotSmall,
 } from "../TagButton";
 import {getTimelinessColor} from "../../helpers/timelinessColor";
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 import getJourneyId from "../../helpers/getJourneyId";
 import {secondsToTimeObject, getNormalTime} from "../../helpers/time";
 import {parseLineNumber} from "../../helpers/parseLineNumber";
@@ -27,6 +27,23 @@ const ListRow = styled.div`
   margin: 0;
   position: relative;
   background: ${({selected = false}) => (selected ? "var(--blue)" : "transparent")};
+
+  ${({isCancelled = false}) =>
+    isCancelled
+      ? css`
+          &:after {
+            content: "";
+            position: absolute;
+            pointer-events: none;
+            height: 2px;
+            background: var(--red);
+            top: 45%;
+            left: 0.5rem;
+            right: 0.5rem;
+            // opacity: 0.7;
+          }
+        `
+      : ""}
 `;
 
 const LineSlot = styled(ColoredSlot)`
@@ -72,7 +89,7 @@ class TimetableDeparture extends Component {
     isTimingStop,
     isSpecialDayType
   ) => (children = null, onClick) => (
-    <ListRow selected={journeyIsSelected}>
+    <ListRow isCancelled={departure.isCancelled} selected={journeyIsSelected}>
       <TimetableButton
         hasData={!!children}
         selected={journeyIsSelected}
