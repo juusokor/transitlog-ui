50ea1d991c1c43525045d1456d945ea84c19cc96
Daniel Dunderfelt <daniel@developsuperpowers.com>
2019-06-24 13:41:43 +0300

    Select env in local build script and use env-specific docker tags.

--- deploy-local.sh
+++ deploy-local.sh
@@ -1,10 +1,41 @@
 #!/bin/bash
 set -e
 
-ORG=${ORG:-hsldevcom}
-DOCKER_IMAGE=${ORG}/transitlog-ui:latest
-DOCKER_IMAGE_LATEST=${ORG}/transitlog-ui:latest
+PS3='Select an environment:'
+option_labels=("Local" "Development" "Staging" "Production")
+select opt in "${option_labels[@]}"; do
+  case $opt in
+  "Local")
+    echo "You chose Local"
+    ENV=local
+    break
+    ;;
+  "Development")
+    echo "You chose Development"
+    ENV=development
+    break
+    ;;
+  "Staging")
+    echo "You chose Staging"
+    ENV=staging
+    break
+    ;;
+  "Production")
+    echo "You chose Production"
+    ENV=production
+    break
+    ;;
+  *)
+    ENV=production
+    break
+    ;;
+  esac
+done
 
-docker build -t ${DOCKER_IMAGE} .
-docker tag ${DOCKER_IMAGE} ${DOCKER_IMAGE_LATEST}
-docker push ${DOCKER_IMAGE_LATEST}
+echo "Building for the $opt ($ENV) environment..."
+
+ORG=${ORG:-hsldevcom}
+DOCKER_IMAGE=${ORG}/transitlog-ui:latest-${ENV}
+
+docker build --build-arg BUILD_ENV=${ENV} -t ${DOCKER_IMAGE} .
+docker push ${DOCKER_IMAGE}
--- travis-build.sh
+++ travis-build.sh
@@ -2,11 +2,11 @@
 set -e
 
 ORG=${ORG:-hsldevcom}
-DOCKER_TAG=${TRAVIS_BUILD_NUMBER:-latest}
+DOCKER_TAG=${TRAVIS_BUILD_NUMBER:-latest-production}
 DOCKER_IMAGE=${ORG}/transitlog-ui:${DOCKER_TAG}
 DOCKER_IMAGE_LATEST=${ORG}/transitlog-ui:latest
 
-docker build -t ${DOCKER_IMAGE} .
+docker build --build-arg BUILD_ENV=production -t ${DOCKER_IMAGE} .
 
 if [[ ${TRAVIS_PULL_REQUEST} == "false" ]] && [[ ${TRAVIS_BRANCH} == "master" ]]; then
   docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
